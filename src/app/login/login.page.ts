18d39c1e30858f15eb06fbe6dc271e8b047e765c
Bruno Gigante <brunogigante2001@gmail.com>
2022-05-25 18:05:50 +0100

    login validate

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm : FormGroup;
+
+  constructor( private formBuilder: FormBuilder ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  logForm(){
+    console.log(this.loginForm.value)
+  }
 
   ngOnInit() {
+    /* this.loginForm.valueChanges.subscribe(
+      (banana)=>{console.log(banana)}
+    ) */
   }
 
 }
