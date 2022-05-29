90225d350b5c71b398720345a8ba30ca9c9e6a76
João Martinho <74744920+Varrys@users.noreply.github.com>
2022-05-29 15:23:02 +0100

    verificação de formulários ao publicar

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,8 +1,7 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
@@ -10,11 +9,12 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
   imports: [
-    IonicModule,
     CommonModule,
     FormsModule,
+    IonicModule,
+    Tab2PageRoutingModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    ReactiveFormsModule
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  addForm : FormGroup;
+
+  constructor( private formBuilder: FormBuilder ) {
+    this.addForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      opinion: ['', Validators.required],
+    });
+  }
+  aForm(){
+    console.log(this.addForm.value)
+  }
+
+  ngOnInit() {
+    /* this.loginForm.valueChanges.subscribe(
+      (banana)=>{console.log(banana)}
+    ) */
+  }
 
 }
