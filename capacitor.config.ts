83a09cf9f34f30b2c9cb1a7569a8c133f06f7045
Bruno Gigante <brunogigante2001@gmail.com>
2022-05-30 21:47:29 +0100

    idk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'pt.ipvc.liber',
   appName: 'liber',
   webDir: 'www',
   bundledWebRuntime: false
