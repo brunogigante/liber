d46227ef3a44c73e40c8f18717bc2a81fb4704b8
João Martinho <74744920+Varrys@users.noreply.github.com>
2022-05-30 20:50:22 +0100

    fix

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+//import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +10,13 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 export class LoginPage implements OnInit {
 
   loginForm : FormGroup;
-
-  constructor( private formBuilder: FormBuilder, private orientacao: ScreenOrientation ) {
+//constructor( private formBuilder: FormBuilder, private orientacao: ScreenOrientation ) {
+  constructor( private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
     });
-    this.orientacao.lock(this.orientacao.ORIENTATIONS.PORTRAIT);
+    //this.orientacao.lock(this.orientacao.ORIENTATIONS.PORTRAIT);
   }
   logForm(){
     console.log(this.loginForm.value)
